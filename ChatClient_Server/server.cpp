9bb9d97a80b76c8f7b86ae43958bb893edeafa0b
Tatrune <103673252+Tatrune@users.noreply.github.com>
2023-09-26 13:19:01 +0700

    update v1.0

---
+++ ChatClient_Server/server.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <WS2tcpip.h>
+#include <string>
+using namespace std;
+
+#pragma comment (lib,"ws2_32.lib")
+
+void main()
+{
+	// init winsock
+	WSADATA wsData;
+	WORD ver = MAKEWORD(2, 2);
+
+	int wsOk = WSAStartup(ver, &wsData);
+	if (wsOk != 0)
+	{
+		cerr << "can't Initialize winsock! Quitting" << endl;
+		return;
+	}
+
+	//create a socket
+	SOCKET listening = socket(AF_INET, SOCK_STREAM, 0);
+
+	// bind the ip address and port to the socket
+	sockaddr_in hint; //sockaddr_in is a data type in the C++ socket library used to represent the IP address and port number of an endpoint
+	hint.sin_family = AF_INET;
+	hint.sin_port = htons(54000);
+	hint.sin_addr.S_un.S_addr = INADDR_ANY;  //could also use inet_pton
+
+	bind(listening, (sockaddr*)&hint, sizeof(hint));
+
+	//tell me winsock the socket is for listening
+	listen(listening, SOMAXCONN);
+
+	// waiting for connect
+	sockaddr_in client; 
+	int clientSize = sizeof(client);
+
+	SOCKET clientSocket = accept(listening, (sockaddr*)&client, &clientSize);
+
+	char host[NI_MAXHOST]; //client's remote name
+	char service[NI_MAXHOST]; //service (i.e.port) the client is connect on
+
+	ZeroMemory(host, NI_MAXHOST); // same as memset(host, 0, NI_MAXHOST);
+	ZeroMemory(service, NI_MAXSERV);
+	
+	if (getnameinfo((sockaddr*)&client, sizeof(client), host, NI_MAXHOST, service, NI_MAXSERV, 0) == 0)
+	{
+		cout << host << " connect on port " << service << endl;
+	}
+	else
+	{	//get the IP address of the client (from the sin_addr field in sockaddr_in), and save it in the server variable. Then the code enters the client's IP address (stored in the server variable) and the port number the client connects to (from the sin_port field in sockaddr_in)
+		inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
+		cout << host << "connected on port" << ntohs(client.sin_port) << endl;
+	}
+
+	//close listening socket
+	closesocket(listening);
+
+	//while loop: accept and echo message back to client
+	char buf[4096];
+	string userInput;
+	while (true)
+	{
+		ZeroMemory(buf, 4096);
+
+		// wait for client to send data
+		int bytesReceived = recv(clientSocket, buf, 4096, 0);
+		if (bytesReceived == SOCKET_ERROR)
+		{
+			cerr << "Error in recv(). quitting" << endl;
+			break;
+		}
+
+		if (bytesReceived == 0)
+		{
+			cout << "client disconnected" << endl;
+			break;
+		}
+		// display on sever
+		cout << "danhan: " << buf << endl;
+
+		cout << ">";
+		getline(cin, userInput);
+		send(clientSocket, userInput.c_str(), userInput.size() + 1, 0);
+
+		// Echo massage back to client
+		//send(clientSocket, buf, bytesReceived + 1, 0);
+
+		
+	}
+
+	//close socket
+	closesocket(clientSocket);
+
+	//cleanup winsock
+	WSACleanup();
+}
---
+++ client/client.cpp
@@ -0,0 +1,116 @@
+﻿#include <iostream>
+#include <string>
+#include <WS2tcpip.h>
+#include <fstream>
+#pragma comment(lib, "ws2_32.lib")
+
+using namespace std;
+
+void main()
+{
+	string ipAddress = "127.0.0.1";			// IP Address of the server
+	int port = 54000;						// Listening port # on the server
+
+	// Initialize WinSock
+	WSAData data;
+	WORD ver = MAKEWORD(2, 2);
+	int wsResult = WSAStartup(ver, &data);
+	if (wsResult != 0)
+	{
+		cerr << "Can't start Winsock, Err #" << wsResult << endl;
+		return;
+	}
+
+	// Create socket
+	SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock == INVALID_SOCKET)
+	{
+		cerr << "Can't create socket, Err #" << WSAGetLastError() << endl;
+		WSACleanup();
+		return;
+	}
+
+	// Fill in a hint structure
+	sockaddr_in hint;
+	hint.sin_family = AF_INET;
+	hint.sin_port = htons(54000);
+	inet_pton(AF_INET, ipAddress.c_str(), &hint.sin_addr);
+
+	// Connect to server
+	int connResult = connect(sock, (sockaddr*)&hint, sizeof(hint));
+	if (connResult == SOCKET_ERROR)
+	{
+		cerr << "Can't connect to server, Err #" << WSAGetLastError() << endl;
+		closesocket(sock);
+		WSACleanup();
+		return;
+	}
+
+	// Do-while loop to send and receive data
+	//char buf[4096];
+	//string userInput;
+
+	//do
+	//{
+	//	// Prompt the user for some text
+	//	cout << ">";
+	//	getline(cin, userInput);
+
+	//	if (userInput.size() > 0)		// Make sure the user has typed in something
+	//	{
+	//		// Send the text
+	//		int sendResult = send(sock, userInput.c_str(), userInput.size() + 1 , 0);
+	//		if (sendResult != SOCKET_ERROR)
+	//		{
+	//			// Wait for response
+
+	//			ZeroMemory(buf, 4096);
+	//			int bytesReceived = recv(sock, buf, 4096, 0); //Waiting...
+	//			if (bytesReceived > 0)
+	//			{
+	//				// Echo response to console
+	//				cout << "Sent: > " << string(buf, 0, bytesReceived) << endl;
+	//			}
+	//		}
+	//	}
+	//} while (userInput.size() > 0);
+
+	// gửi tên file
+	/*char nameFile[] = "hinh.rar";
+	send(sock, (char*)nameFile, sizeof(nameFile), 0);*/
+
+	char nameFile[] = "hinh.rar";
+	send(sock, (char*)nameFile, strlen(nameFile), 0);
+
+	ifstream file("C:\\Users\\ACER NITRO 5\\Desktop\\Tcp_protocol\\client\\hinh.rar", ios::binary);
+	file.seekg(0, ios::end);
+	int size = file.tellg();
+	file.seekg(0, ios::beg);
+	char* buffer = new char[size];
+	file.read(buffer, size);
+	file.close();
+
+	// gửi kích thước file (binary)
+	int* fsize = &size;
+	int err = send(sock, (char*)fsize, sizeof(fsize), 0);
+	cout << "size: " << size << endl;
+	if (err <= 0)
+	{
+		printf("send: %d\n", WSAGetLastError());
+	}
+	printf("send %d bytes [OK]\n", err);
+
+	// gửi data (binary) 
+	err = send(sock, buffer, size, 0);
+	if (err <= 0)
+	{
+		printf("send: %d\n", WSAGetLastError());
+	}
+	printf("send %d bytes [OK]\n", err);
+	cout << "data: " << buffer << endl;
+	delete[] buffer;
+
+	// Gracefully close down everything
+	closesocket(sock);
+	WSACleanup();
+}
